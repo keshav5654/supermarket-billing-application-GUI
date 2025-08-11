625371a210d22ebdb3492c5af84f5ed44a67e9d5
keshav5654 <keshavmuraripctebca21a@gmail.com>
2025-08-11 19:31:02 +0530

    supermarket-billing-app-gui-Java

---
+++ Billingsystem/src/main/java/billingsystem/Billingsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package billingsystem;
+
+/**
+ *
+ * @author kesha
+ */
+public class Billingsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
